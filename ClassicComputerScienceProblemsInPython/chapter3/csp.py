fa82972cfd36cbbf81322b165e61030914d3a51b
Kanbe Hiroyuki <kanbe.hmw@gmail.com>
2019-06-20 22:46:54 +0900

    solution scripts of constraint-satisfaction problem

--- ClassicComputerScienceProblemsInPython/chapter3/csp.py
+++ ClassicComputerScienceProblemsInPython/chapter3/csp.py
@@ -23,4 +23,29 @@ class CSP(Generic[V, D]):
                 raise LookupError("Every variable should have a domain assigned to it.")
 
     def add_constraint(self, constraint: Constraint[V, D]) -> None:
-        pass
+        for variable in constraint.variables:
+            if variable not in self.variables:
+                raise LookupError("Variable in constraint not in CSP")
+            else:
+                self.constraints[variable].append(constraint)
+
+    def consistent(self, variable: V, assignment: Dict[V, D]) -> bool:
+        for constraint in self.constraints[variable]:
+            if not constraint.satisfied(assignment):
+                return False
+        return True
+
+    def backtracking_search(self, assignment: Dict[V, D] = {}) -> Optional[Dict[V, D]]:
+        if len(assignment) == len(self.variables):
+            return assignment
+
+        unassigned: List[V] = [v for v in self.variables if v not in assignment]
+        first: V = unassigned[0]
+        for value in self.domains[first]:
+            local_assignment = assignment.copy()
+            local_assignment[first] = value
+            if self.consistent(first, local_assignment):
+                result: Optional[Dict[V, D]] = self.backtracking_search(local_assignment)
+                if result is not None:
+                    return result
+        return None
