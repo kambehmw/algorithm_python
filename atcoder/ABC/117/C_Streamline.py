df801f195f29eb25d5efd3a379d78976b654bbc1
kanbehmw <kanbe.hmw@gmail.com>
2019-07-17 20:31:01 +0900

    simple version

--- atcoder/ABC/117/C_Streamline.py
+++ atcoder/ABC/117/C_Streamline.py
@@ -2,20 +2,8 @@ N, M = map(int, input().split())
 X = list(map(int, input().split()))
 
 X = sorted(X)
-diff_dict = {}
+diff = []
 for i in range(M-1):
-    diff = X[i+1] - X[i]
-    diff_dict[i] = diff
-sorted_diff = sorted(diff_dict.items(), key=lambda x: x[1])[::-1][:N-1]
-keys = {x[0] for x in sorted_diff}
-tmp = []
-count = 0
-for i, x in enumerate(X):
-    tmp.append(x)
-    if i in keys:
-        if len(tmp) > 1:
-            count += (max(tmp) - min(tmp))
-        tmp.clear()
-if len(tmp) > 1:
-    count += (max(tmp) - min(tmp))
-print(count)
+    diff.append(X[i+1] - X[i])
+diff = sorted(diff)[::-1]
+print(X[-1] - X[0] - sum(diff[:N-1]))
