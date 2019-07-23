52fe7120d9e51f76108b9c39365d4b5f745a3ec6
kanbehmw <kanbe.hmw@gmail.com>
2019-07-23 20:05:53 +0900

    B solution in ABC 100

---
+++ atcoder/ABC/100/B_Ringo's_Favorite_Numbers.py
@@ -0,0 +1,14 @@
+def calc(x):
+    ret = 0
+    while x % 100 == 0:
+        x /= 100
+        ret += 1
+    return ret
+
+D, N = map(int, input().split())
+cnt, val = 0, 0
+while cnt < N:
+    val += 1
+    if calc(val) == D:
+        cnt += 1
+print(val)
