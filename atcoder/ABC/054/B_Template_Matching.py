a91c7745406b7a0ea8921723d733d184092a4185
Kanbe Hiroyuki <kanbe.hmw@gmail.com>
2019-09-28 23:55:25 +0900

    B solution in ABC 054

---
+++ atcoder/ABC/054/B_Template_Matching.py
@@ -0,0 +1,14 @@
+N, M = tuple(map(int, input().split()))
+A = [input() for _ in range(N)]
+B = [input() for _ in range(M)]
+for i in range(N-M+1):
+    for j in range(N-M+1):
+        flag = True
+        for y in range(M):
+            for x in range(M):
+                if A[i+y][j+x] != B[y][x]:
+                    flag = False
+        if flag:
+            print("Yes")
+            exit()
+print("No")
