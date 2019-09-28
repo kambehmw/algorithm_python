8caa20138c0e8f84a7477dfa66961eda3bca9110
Kanbe Hiroyuki <kanbe.hmw@gmail.com>
2019-09-28 23:37:42 +0900

    B solution

---
+++ atcoder/tenka1_programmer_beginner_contest/B_Exchange.py
@@ -0,0 +1,13 @@
+A, B, K = tuple(map(int, input().split()))
+for i in range(K):
+    if i % 2 == 0:
+        if A % 2 != 0:
+            A -= 1
+        B += A // 2
+        A = A // 2
+    else:
+        if B % 2 != 0:
+            B -= 1
+        A += B // 2
+        B = B // 2
+print(A, B)
