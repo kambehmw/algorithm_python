972cbd188bcbd6e3b09dd0e660454e8bc235aa95
Kanbe Hiroyuki <kanbe.hmw@gmail.com>
2020-01-05 11:47:17 +0900

    fix minor code

--- atcoder/DISCO_Presents_Discovery_Channel_Code_Contest_2020_Qual/A_DDCC_Finals.py
+++ atcoder/DISCO_Presents_Discovery_Channel_Code_Contest_2020_Qual/A_DDCC_Finals.py
@@ -1,22 +1,22 @@
 X, Y = tuple(map(int, input().split()))
 
 # Calculate prize for "Coding Contest"
-prize_code = 0;
+prize_code = 0
 if X == 1:
-    prize_code = 300000;
+    prize_code = 300000
 elif X == 2:
-    prize_code = 200000;
+    prize_code = 200000
 elif X == 3:
-    prize_code = 100000;
+    prize_code = 100000
 
 # Calculate prize for "Robot Maneuver"
-prize_device = 0;
+prize_device = 0
 if Y == 1:
-    prize_device = 300000;
+    prize_device = 300000
 elif Y == 2:
-    prize_device = 200000;
+    prize_device = 200000
 elif Y == 3:
-    prize_device = 100000;
+    prize_device = 100000
 
 # Calculate prize for "two victories"
 prize_both = 0
