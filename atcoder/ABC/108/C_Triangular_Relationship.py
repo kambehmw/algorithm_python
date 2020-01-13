6dd92a870c3af0af5943c165afeba128c95fd3be
Kanbe Hiroyuki <kanbe.hmw@gmail.com>
2020-01-14 00:07:02 +0900

    C solution of ABC 108

---
+++ atcoder/ABC/108/C_Triangular_Relationship.py
@@ -0,0 +1,12 @@
+N, K = tuple(map(int, input().split()))
+num = [0] * K
+for i in range(1, N+1):
+    num[i % K] += 1
+res = 0
+for a in range(K):
+    b = (K - a) % K
+    c = (K - a) % K
+    if (b + c) % K != 0:
+        continue
+    res += num[a] * num[b] * num[c]
+print(res)
