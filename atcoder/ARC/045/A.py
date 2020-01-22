0edf1ce90c8cebd8b043ea04ed8fbc6a622b724f
Kambe Hiroyuki <kanbe.hmw@gmail.com>
2020-01-22 11:21:31 +0900

    A solution

---
+++ atcoder/ARC/045/A.py
@@ -0,0 +1,10 @@
+S = input().split()
+ans = []
+for s in S:
+    if s == "Left":
+       ans.append("<")
+    elif s == "Right":
+        ans.append(">")
+    elif s == "AtCoder":
+        ans.append("A")
+print(" ".join(ans))
