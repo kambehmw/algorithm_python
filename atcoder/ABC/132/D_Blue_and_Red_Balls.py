2494587fede63fb230cd509dae5fceeb80e2c5c9
Kanbe Hiroyuki <kanbe.hmw@gmail.com>
2020-02-29 23:25:04 +0900

    D solution

--- atcoder/ABC/132/D_Blue_and_Red_Balls.py
+++ atcoder/ABC/132/D_Blue_and_Red_Balls.py
@@ -1,21 +1,25 @@
-# as this method is slow, time out error occurred
-def factorial(n):
-    total = 1
-    for i in range(1, n+1):
-        total *= i
-    return total
+def cmb(n, r, p):
+    if (r < 0) or (n < r):
+        return 0
+    r = min(r, n - r)
+    return fact[n] * factinv[r] * factinv[n - r] % p
 
-def combinations_count(n, r):
-    return factorial(n) // (factorial(n - r) * factorial(r))
 
-MOD = 1000000000 + 7
-def main():
-    N, K = list(map(int, input().split()))
+p = 10 ** 9 + 7
+N = 2000  # N は必要分だけ用意する
+fact = [1, 1]  # fact[n] = (n! mod p)
+factinv = [1, 1]  # factinv[n] = ((n!)^(-1) mod p)
+inv = [0, 1]  # factinv 計算用
 
-    for k in range(1, K+1):
-        res = combinations_count(N-K+1, k)
-        res *= combinations_count(K-1, k-1)
-        print(res % MOD)
+for i in range(2, N + 1):
+    fact.append((fact[-1] * i) % p)
+    inv.append((-inv[p % i] * (p // i)) % p)
+    factinv.append((factinv[-1] * inv[-1]) % p)
 
-if __name__ == '__main__':
-    main()
+
+N, K = list(map(int, input().split()))
+
+for k in range(1, K + 1):
+    res = cmb(N - K + 1, k, p)
+    res *= cmb(K - 1, k - 1, p)
+    print(res % p)
