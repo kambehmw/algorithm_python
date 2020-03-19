50964547dc813434e1410e936ca17d173759a9a1
Kanbe Hiroyuki <kanbe.hmw@gmail.com>
2020-03-19 23:00:58 +0900

    C solution

--- atcoder/ABC/031/C.py
+++ atcoder/ABC/031/C.py
@@ -5,18 +5,15 @@ for i in range(N):
     max_score = float('-inf')
     max_start, max_end = None, None
     for j in range(N):
-        if j < i:
-            start = j
-            end = i
-        else:
-            start = i
-            end = j
-
-        if start == end:
+        if i == j:
             continue
-        score = sum(A[start:end][1::2])
+        if i < j:
+            start, end = i, j
+        else:
+            start, end = j, i
+        score = sum(A[start:end+1][1::2])
         if max_score < score:
             max_score = score
             max_start, max_end = start, end
-    ans = max(ans, sum(A[max_start:max_end][0::2]))
+    ans = max(ans, sum(A[max_start:max_end+1][::2]))
 print(ans)
