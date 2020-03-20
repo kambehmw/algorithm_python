56630e4fe03a2dd28a25d88d33e0c33092de190f
Kanbe Hiroyuki <kanbe.hmw@gmail.com>
2020-03-21 00:20:44 +0900

    B solution

---
+++ atcoder/ARC/046/B.py
@@ -0,0 +1,15 @@
+N = int(input())
+A, B = map(int, input().split())
+if N <= A:
+    print("Takahashi")
+else:
+    if A == B:
+        if N % (A + 1) == 0:
+            print("Aoki")
+        else:
+            print("Takahashi")
+    else:
+        if A > B:
+            print("Takahashi")
+        else:
+            print("Aoki")
