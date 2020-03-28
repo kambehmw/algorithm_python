d5cd085b7aa710218260bef965dc683dac487dec
Kanbe Hiroyuki <kanbe.hmw@gmail.com>
2020-03-28 19:48:01 +0900

    A solution

---
+++ atcoder/tenka1_programmer_contest_2012_qual_C/A.py
@@ -0,0 +1,14 @@
+def primes(n):
+    if n < 2:
+        return []
+    is_prime = [True] * (n + 1)
+    is_prime[0] = False
+    is_prime[1] = False
+    for i in range(2, n + 1):
+        for j in range(i * 2, n + 1, i):
+            is_prime[j] = False
+    return [i for i in range(n + 1) if is_prime[i]]
+
+N = int(input())
+ans = primes(N - 1)
+print(len(ans))
