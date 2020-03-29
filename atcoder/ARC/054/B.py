d82bfb2a72737e8186e5d48d899c85d680120b8a
Kanbe Hiroyuki <kanbe.hmw@gmail.com>
2020-03-29 17:00:34 +0900

    B solution

---
+++ atcoder/ARC/054/B.py
@@ -0,0 +1,17 @@
+P = float(input())
+
+
+def f(x):
+    return x + P / 2 ** (x / 1.5)
+
+
+high = 100
+low = 0
+while high - low > 0.000000001:
+    mid_left = high / 3 + low * 2 / 3
+    mid_right = high * 2 / 3 + low / 3
+    if f(mid_left) >= f(mid_right):
+        low = mid_left
+    else:
+        high = mid_right
+print(f(high))
