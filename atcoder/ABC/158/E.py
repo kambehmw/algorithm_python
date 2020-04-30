c12f25f1f30d8019ac0c87b6f720f061b980a478
Kanbe Hiroyuki <kanbe.hmw@gmail.com>
2020-04-30 23:16:02 +0900

    E solution

--- atcoder/ABC/158/E.py
+++ atcoder/ABC/158/E.py
@@ -1,16 +1,24 @@
 N, P = map(int, input().split())
 S = input()
-numMultiples = 0
-seenRemainders = [set() for _ in range(N)]
-for i in range(N):
-    remainder = 0
-    prefixesFound = 0
-    for j in range(i, N):
-        remainder = (10 * remainder + int(S[j])) % P
-        if remainder in seenRemainders[j]:
-            break
-        seenRemainders[j].add(remainder)
-        if remainder == 0:
-            prefixesFound += 1
-    numMultiples += prefixesFound * (prefixesFound + 1) // 2
-print(numMultiples)
+if 10 % P == 0:
+    ans = 0
+    for r in range(N):
+        if int(S[r]) % P == 0:
+            ans += r + 1
+    print(ans)
+    exit()
+
+d = [0] * (N + 1)
+ten = 1
+for i in range(N - 1, -1, -1):
+    a = int(S[i]) * ten % P
+    d[i] = (d[i + 1] + a) % P
+    ten *= 10
+    ten %= P
+
+cnt = [0] * P
+ans = 0
+for i in range(N, -1, -1):
+    ans += cnt[d[i]]
+    cnt[d[i]] += 1
+print(ans)
