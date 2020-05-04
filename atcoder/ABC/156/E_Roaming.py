203812847462d37790486e3ec973935cabb3fc55
Kanbe Hiroyuki <kanbe.hmw@gmail.com>
2020-05-04 20:10:32 +0900

    E solution

---
+++ atcoder/ABC/156/E_Roaming.py
@@ -0,0 +1,25 @@
+def cmb(n, r, p):
+    if (r < 0) or (n < r):
+        return 0
+    r = min(r, n - r)
+    return fact[n] * factinv[r] * factinv[n - r] % p
+
+
+p = 10 ** 9 + 7
+N = 2 * 10 ** 5  # N は必要分だけ用意する
+fact = [1, 1]  # fact[n] = (n! mod p)
+factinv = [1, 1]  # factinv[n] = ((n!)^(-1) mod p)
+inv = [0, 1]  # factinv 計算用
+
+for i in range(2, N + 1):
+    fact.append((fact[-1] * i) % p)
+    inv.append((-inv[p % i] * (p // i)) % p)
+    factinv.append((factinv[-1] * inv[-1]) % p)
+
+n, k = map(int, input().split())
+ans = 0
+k = min(k, n - 1)
+for i in range(k+1):
+    ans += cmb(n, i, p) * cmb(n - 1, i, p)
+    ans %= p
+print(ans % p)
