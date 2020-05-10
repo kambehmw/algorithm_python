6ea61638fe4ab7d23ab292b4f5eb2c6471edea1f
Kanbe Hiroyuki <kanbe.hmw@gmail.com>
2020-05-11 00:38:17 +0900

    A solution

---
+++ codeforces/contest/Educational_Codeforces_Round/086/A.py
@@ -0,0 +1,8 @@
+t = int(input())
+for i in range(t):
+    x, y = map(int, input().split())
+    a, b = map(int, input().split())
+    b = min(b, a + a)
+    if x < y:
+        x, y = y, x
+    print(y * b + (x - y) * a)
